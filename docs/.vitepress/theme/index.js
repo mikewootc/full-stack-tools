845d959afec67458b7179f6f3a6431f53115d4af
mike <mike@MikeThinkWls.com>
2025-05-09 09:46:44 +0800

    Add busuanzi.

---
+++ docs/.vitepress/theme/index.js
@@ -0,0 +1,17 @@
+import DefaultTheme from 'vitepress/theme'
+
+import { inBrowser } from 'vitepress'
+import busuanzi from 'busuanzi.pure.js'
+
+export default {
+  extends: DefaultTheme,
+
+  enhanceApp({ app , router }) {
+    if (inBrowser) {
+      router.onAfterRouteChanged = () => {
+        busuanzi.fetch()
+      }
+    }
+  },
+  
+}
